c6a903c0a5bfbf8c4b888f0d3da022bdf958051f
Kenneth Foner <kenny.foner@gmail.com>
2013-11-10 17:28:07 -0500

    Slight reformatting

--- ifold.hs
+++ ifold.hs
@@ -24,14 +24,16 @@ parseWithDefault d =
 
 main :: IO ()
 main = do
+
    progName <- getProgName
    argv <- getArgs
+
    let usageMessage = "Usage: " ++ progName ++ " [integer line width] [integer tab size]"
+   
    (w,t) <- case argv of
       []        -> return (defaultLineWidth,defaultTabSize)
       [w]       -> return (parseWithDefault defaultLineWidth w,defaultTabSize)
       [w,t]     -> return (parseWithDefault defaultLineWidth w,parseWithDefault defaultTabSize t)
       otherwise -> ioError (userError usageMessage)
 
-   runEffect $
-      for P.stdinLn (mapM_ yield . foldLine t w) >-> P.stdoutLn
+   runEffect $ for P.stdinLn (mapM_ yield . foldLine t w) >-> P.stdoutLn
